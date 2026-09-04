2a91256d77a34dd3bb94110462715dd024d2500d
Atharv Dahake <atharvdahake7@gmail.com>
2026-09-04 16:31:21 +0530

    Add files via upload

---
+++ vite.config.js
@@ -0,0 +1,20 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import { resolve } from 'path'
+
+export default defineConfig({
+  plugins: [react()],
+
+  server: {
+    port: 3000
+  },
+
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(process.cwd(), 'index.html'),
+        admin: resolve(process.cwd(), 'admin.html')
+      }
+    }
+  }
+})
